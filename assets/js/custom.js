898cd30be1fbfa0ef878fe8664f27924e18a4d6c
devavology <145644105+devavology@users.noreply.github.com>
2024-07-01 18:04:51 +0530

    first commit

---
+++ assets/js/custom.js
@@ -0,0 +1,56 @@
+// $(".toggle").click(function(){
+
+//   $("header").toggleClass("menuToggle");
+
+// });
+
+$(".toggle").click(function() {
+    $("header").addClass("menuToggle");
+});
+$(".close").click(function() {
+    $("header").removeClass("menuToggle");
+});
+
+
+
+
+
+$(window).scroll(function(){
+    if ($(window).scrollTop() >= 30) {
+        $('header').addClass('fixed-header');
+        $('header').addClass('visible-title');
+    }
+    else {
+        $('header').removeClass('fixed-header');
+        $('header').removeClass('visible-title');
+    }
+});
+
+
+
+
+
+
+
+$('.contless-button').click(function() {
+  $(this).prev('.moretextt').slideToggle();
+  if ($('.contless-button').text() == "Veja mais") {
+    $(this).text("veja menos")
+  } else {
+    $(this).text("Veja mais")
+  }
+});
+
+
+
+$('.moreless-button').click(function() {
+  $('.moretext').slideToggle();
+  if ($('.moreless-button').text() == "Veja mais") {
+    $(this).text("veja menos")
+  } else {
+    $(this).text("Veja mais")
+  }
+});
+AOS.init({
+    duration: 1200,
+  })
