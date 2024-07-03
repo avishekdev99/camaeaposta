2f1356961e4a0e555f35e8880ba3f56c2a41ad25
devavology <145644105+devavology@users.noreply.github.com>
2024-07-03 12:56:43 +0530

    done

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -32,12 +32,22 @@ $(window).scroll(function(){
 
 
 
+// $('.contless-button').click(function() {
+//   $(this).prev('.moretextt').slideToggle();
+//   if ($(this).html() == 'Veja mais <i class="fa-solid fa-angle-down"></i>') {
+//     $(this).html('Veja menos');
+//   } else {
+//     $(this).html('Veja mais <i class="fa-solid fa-angle-down"></i>');
+//   }
+// });
+
 $('.contless-button').click(function() {
   $(this).prev('.moretextt').slideToggle();
-  if ($('.contless-button').text() == "Veja mais") {
-    $(this).text("veja menos")
+  
+  if ($(this).html().includes('Veja mais')) {
+    $(this).html('Veja menos <i class="fas fa-angle-up"></i>');
   } else {
-    $(this).text("Veja mais")
+    $(this).html('Veja mais <i class="fas fa-angle-down"></i>');
   }
 });
 
